4e11d7fc0c2b3afe1a2bb6a2d22db5652868f449
buon1ngay <buon1ngay@gmail.com>
2025-11-24 22:14:40 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -56,7 +56,7 @@ QUY TẮC:
           content: extractionPrompt
         }
       ],
-      model: 'llama-3.3-70b-versatile',
+      model: 'llama-3.1-70b-versatile',
       temperature: 0.3,
       max_tokens: 500
     });
