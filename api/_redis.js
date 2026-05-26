b7d3fd4acd2a72f6f4c0b395a2ddb6298a37d858
buon1ngay <buon1ngay@gmail.com>
2026-05-26 16:06:06 +0700

    New

--- api/_redis.js
+++ api/_redis.js
@@ -1,13 +1,15 @@
-import { Redis } from '@upstash/redis'
+const { Redis } = require('@upstash/redis');
 
-let redis = null
+let redis = null;
 
-export function getRedis() {
+function getRedis() {
   if (!redis) {
     redis = new Redis({
       url: process.env.UPSTASH_REDIS_REST_URL,
       token: process.env.UPSTASH_REDIS_REST_TOKEN,
-    })
+    });
   }
-  return redis
+  return redis;
 }
+
+module.exports = { getRedis };
